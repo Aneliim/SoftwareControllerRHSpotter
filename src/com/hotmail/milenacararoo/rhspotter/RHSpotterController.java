959c6f387174173c9713283237d4ed3a6ea5e45a
Milena Cararo <milenacararoo@hotmail.com>
2015-12-01 13:29:45 -0200

    Tela para escolha de porta.

--- src/com/hotmail/milenacararoo/rhspotter/RHSpotterController.java
+++ src/com/hotmail/milenacararoo/rhspotter/RHSpotterController.java
@@ -2,10 +2,10 @@ package com.hotmail.milenacararoo.rhspotter;
 
 import java.net.URL;
 
-import br.com.spotdev.robotcontroller.Comunicacao;
-import br.com.spotdev.robotcontroller.RHSpotter;
-import br.com.spotdev.robotcontroller.communication.RequestCallback;
-import br.com.spotdev.robotcontroller.communication.exceptions.ConnectionException;
+//import br.com.spotdev.robotcontroller.Comunicacao;
+//import br.com.spotdev.robotcontroller.RHSpotter;
+//import br.com.spotdev.robotcontroller.communication.RequestCallback;
+//import br.com.spotdev.robotcontroller.communication.exceptions.ConnectionException;
 /*-----------------------------------------*/
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -20,30 +20,6 @@ public class RHSpotterController extends Application {
 	 *  MÉTODO MAIN
 	 */
 	public static void main(String[] args) {
-		Comunicacao comunicacao = new Comunicacao("COM8");
-		// abre o painel de carregamento para se coenctar no robo
-		try {
-			comunicacao.iniciar(new RequestCallback() {
-				
-				@Override
-				public void success(RHSpotter rhspotter) {
-					// TODO Auto-generated method stub
-					// aqui chama quando conecta
-					// conectado com sucesso, pode fechar o painel
-				}
-				
-				@Override
-				public void error(String mensagem) {
-					// TODO Auto-generated method stub
-					// Aqui chama quando qu você não se conecta com o robo
-					// erro ao se conectar, omostrar mensagem
-					System.out.println(mensagem);
-				}
-			} );
-		} catch (ConnectionException e) {
-			// Aqui e chamado quando voce não consegue conectar o modulo no seu computador direito
-			e.printStackTrace();
-		}
 		launch();
 	}
 
